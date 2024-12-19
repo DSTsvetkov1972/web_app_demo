089cabc9936f9a25e12ff39607c14fd5814c6d06
Цветков Дмитрий Сергеевич <tsvetkovds@trcont.ru>
2024-12-19 14:26:40 +0300

    improove the code using flake8

--- service/app/ui.py
+++ service/app/ui.py
@@ -1,20 +1,16 @@
-from colorama import Fore 
+# from colorama import Fore
 import asyncio
-from ui_fns import init_app, on_login, files_uploader, show_tasks_status, show_user_tasks, show_ready_user_tasks
-import global_vars
+from ui_fns import (init_app, on_login, files_uploader,
+                    show_tasks_status, show_user_tasks,
+                    show_ready_user_tasks)
+
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
-#input('HTTP_PROXY' in os.environ)
-#input(os.environ['HTTP_PROXY'])
 
-
-#params = st.query_params
-#if 'current_user' in params:
-#    current_user = params['current_user']
 init_app()
 
 if "app_restarted" in st.session_state:
-    st.session_state.app_restarted=True
+    st.session_state.app_restarted = True
 
 if "show_ready" not in st.session_state:
     st.session_state.show_ready = False
@@ -22,36 +18,32 @@ if "show_ready" not in st.session_state:
 current_user_id = 17
 
 
-
-
-
-
 if on_login(current_user_id):
-
-    #user_messages = get_user_messages(current_user_id)
-    #if user_messages[0]:
-    #    show_dialog(user_messages[0])
-    
     files_uploader(current_user_id)
-    #show_earliest_user_task(current_user_id)
-    
 
-    # show_tasks_status()
 if st.session_state.show_ready:
-    st.button("Очередь задач", on_click=lambda:streamlit_js_eval(js_expressions="parent.window.location.reload()") )
+    col1, col2 = st.columns(2)
+    with col1:    
+        js_expressions = "parent.window.location.reload()"
+        st.button("Перейти в монитор задач",
+                  on_click=lambda: streamlit_js_eval(js_expressions=js_expressions),
+                  use_container_width=True)
     show_ready_user_tasks(current_user_id)
 else:
     col1, col2 = st.columns(2)
     with col1:
-        st.button("Перейти к выполненным задачам", on_click=lambda d, k, v: d.update({k:v}), args = [st.session_state, 'show_ready', True], use_container_width=True)
+        st.button("Перейти к выполненным задачам",
+                  on_click=lambda d, k, v: d.update({k: v}),
+                  args=[st.session_state, 'show_ready', True],
+                  use_container_width=True)
     with col2:
-        st.button("Остановить обоаботку текущего задания", use_container_width=True, disabled=True, type="primary")
+        st.button("Остановить обоаботку текущего задания",
+                  use_container_width=True,
+                  disabled=True,
+                  type="primary")
+
     async def main():
-        await asyncio.gather(show_user_tasks(current_user_id), show_tasks_status())
-        #await asyncio.gather(show_tasks_status())
-        #await asyncio.gather(show_user_tasks(current_user_id))
+        await asyncio.gather(show_user_tasks(current_user_id),
+                             show_tasks_status())
 
     asyncio.run(main())
-
-
-        
--- service/app/ui_fns.py
+++ service/app/ui_fns.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import os
 import asyncio
@@ -6,10 +5,11 @@ from colorama import Fore
 import threading as th
 from time import sleep
 from models import processor
-from db_fns import init_db, create_user, put_task_in_queue, get_first_taskid_id_in_queue, get_tasks_ids, get_task_info, reset_processing_to_queued, set_db_column_value, get_all_tasks_ids
+from db_fns import (init_db, create_user, put_task_in_queue,
+                     get_first_taskid_id_in_queue, get_tasks_ids, get_task_info,
+                     reset_processing_to_queued, set_db_column_value, get_all_tasks_ids)
 from streamlit_js_eval import streamlit_js_eval
 import global_vars
-
 
 
 def worker():
@@ -21,55 +21,52 @@ def worker():
             if task:
                 processor(task)
                 continue
-
-        # print('ping worker')        
+        # print('ping worker')
         sleep(1)
-    
+
 
 def init_worker_thread():
     reset_processing_to_queued()
     if 'worker_thread' not in [thread.name for thread in th.enumerate()]:
         global_vars.worker_thread = th.Thread(target=worker,
-            name = 'worker_thread') 
+                                              name='worker_thread')
         global_vars.worker_thread.start()
 
+
 def complit_task(task_id):
-    #global_vars.needs_to_reload = True
     print(Fore.RED, "Мы здесь!", Fore.RESET)
 
     task_info = get_task_info(task_id)
     print(Fore.MAGENTA, task_info, Fore.RESET)
 
-
     user_folder = os.path.join(os.getcwd(), 'data', str(task_info['user_id']))
     print(Fore.RED, user_folder, Fore.RESET)
     task_input_file = os.path.join(user_folder, 'input', task_info['file_name'])
-    task_output_file = os.path.join(user_folder, 'output',f'{task_info["file_name"]}.docx')
+    task_output_file = os.path.join(user_folder, 'output', f'{task_info["file_name"]}.docx')
     if task_info['task_status'] == 'ready':
-        print(Fore.MAGENTA, os.path.exists(task_input_file), Fore.RESET)           
+        print(Fore.MAGENTA, os.path.exists(task_input_file), Fore.RESET)
         if os.path.exists(task_input_file):
             os.remove(task_input_file)
         if os.path.exists(task_output_file):
             os.remove(task_output_file)
-           
+
     elif task_info['task_status'] == 'error' and task_info['task_rem'] != 'Already was puted in queue':
-        print(Fore.MAGENTA, os.path.exists(task_input_file), Fore.RESET)      
+        print(Fore.MAGENTA, os.path.exists(task_input_file), Fore.RESET)
         if os.path.exists(task_input_file):
-            print(Fore.MAGENTA, task_input_file, Fore.RESET)               
+            print(Fore.MAGENTA, task_input_file, Fore.RESET)
             os.remove(task_input_file)
 
-        if os.path.exists(task_output_file):            
+        if os.path.exists(task_output_file):
             print(Fore.MAGENTA, task_output_file, Fore.RESET)
-            os.remove(task_output_file) 
-         
-    set_db_column_value(task_id, 'task_status', "'complited'") 
-    
+            os.remove(task_output_file)
+
+    set_db_column_value(task_id, 'task_status', "'complited'")
+
     global_vars.needs_to_reload = True
 
-    
+    # streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
-    #streamlit_js_eval(js_expressions="parent.window.location.reload()") 
-    
+
 def init_app():
     if not global_vars.app_activated:
         init_db()
@@ -79,8 +76,7 @@ def init_app():
 
 def on_login(current_user_id):
 
-
-    current_user_folder = os.path.join(os.getcwd(),'data', f"{current_user_id}")
+    current_user_folder = os.path.join(os.getcwd(), 'data', f"{current_user_id}")
     current_user_input_folder = os.path.join(current_user_folder, 'input')
     current_user_output_folder = os.path.join(current_user_folder, 'output')
 
@@ -88,29 +84,28 @@ def on_login(current_user_id):
     print(current_user_input_folder)
     print(current_user_output_folder)
 
-
     if not os.path.exists(current_user_folder):
-        os.makedirs(current_user_folder)   
-    if not os.path.exists(current_user_input_folder):             
+        os.makedirs(current_user_folder)
+    if not os.path.exists(current_user_input_folder):
         os.makedirs(current_user_input_folder)
-    if not os.path.exists(current_user_output_folder):             
+    if not os.path.exists(current_user_output_folder):
         os.makedirs(current_user_output_folder)
 
-    create_user(current_user_id)   
+    create_user(current_user_id)
     with st.sidebar:
         st.write("**Оператор:**")
         st.text_input("Оператор:", current_user_id, disabled=True, label_visibility='collapsed')
         st.button('Выйти из аккаунта', disabled=True, use_container_width=True)
-    
+
     return True
 
 
-
-
 def files_uploader(user_id):
-    with st.sidebar: 
-        with st.expander(f'Загрузите файлы для обработки:'):
-            uploaded_files = st.file_uploader("Загрузите файлы для обработки", accept_multiple_files=True, label_visibility="collapsed")#, accept_multiple_files=False)
+    with st.sidebar:
+        with st.expander('Загрузите файлы для обработки:'):
+            uploaded_files = st.file_uploader("Загрузите файлы для обработки",
+                                              accept_multiple_files=True,
+                                              label_visibility="collapsed")
             if uploaded_files != []:
 
                 for uploaded_file in uploaded_files:
@@ -124,44 +119,43 @@ def files_uploader(user_id):
                         put_task_in_queue(user_id, file_name, task_status='queued')
                     else:
                         put_task_in_queue(user_id, file_name, task_status='error')
-            
-                #global_vars.needs_to_reload = True
+
                 streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
-                        
-                    
 
 async def show_tasks_status():
 
-
     while True:
         with st.sidebar:
-            #print(f'needs_to_reload {global_vars.needs_to_reload}')
-            placeholder = st.empty()       
+            # print(f'needs_to_reload {global_vars.needs_to_reload}')
+            placeholder = st.empty()
             tasks_ids = get_all_tasks_ids()
             with placeholder.container(border=True):
                 if tasks_ids:
                     for task_id in tasks_ids[:5]:
                         task_info = get_task_info(task_id[0])
-                        #st.write(get_tasks_ids())
+                        # st.write(get_tasks_ids())
 
-                        st.header(f"**Задача: {task_info['user_id']}-{task_info['task_id']}**")
-                        if task_info['task_status']=='processing':
+                        st.header(f"**Задача: {task_info['user_id']}"
+                                  f"-{task_info['task_id']}**")
+                        if task_info['task_status'] == 'processing':
 
                             if task_info['task_progress']:
                                 st.progress(int(task_info['task_progress']))
-                            st.html(f'<b>В очереди с:</b><br><font color="grey">{task_info["task_timestamp"][:19]}<br><b><font color="black">Обработка стартовала:</b><br><font color="grey">{task_info["task_timestamp"][:19]}')
-                            #st.html(f'<font color="grey">{task_info["task_timestamp"][:19]}<br><b><font color="black">Обработка стартовала:</b><br><font color="grey">{task_info["task_timestamp"][:19]}')                            
+                            st.html(f'<b>В очереди с:</b><br><font color="grey">'
+                                    f'{task_info["task_timestamp"][:19]}<br>'
+                                    f'<b><font color="black">Обработка стартовала:</b>'
+                                    f'<br><font color="grey">{task_info["task_timestamp"][:19]}')
+
                         else:
                             st.html(f'<b>В очереди с:</b><br><font color="grey">{task_info["task_timestamp"][:19]}')
-
 
         await asyncio.sleep(0.5)
         placeholder.empty()
 
 
 async def show_user_tasks(current_user_id):
-    placeholder = st.empty() 
+    placeholder = st.empty()
     i = 0
     while True:
         i += 1
@@ -175,73 +169,72 @@ async def show_user_tasks(current_user_id):
                         if task_info['task_status'] == 'queued':
                             st.markdown(f'#### :orange[{current_user_id}-{task_id[0]}]')
                             st.markdown(f'### :orange[{task_info["file_name"]} ожидает обработки]')
-                            st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}')  
+                            st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}')
 
                         if task_info['task_status'] == 'error':
                             st.markdown(f'#### :red[{current_user_id}-{task_id[0]}]')
                             st.markdown(f'### :red[{task_info["file_name"]} - ошибка обработки]')
-                            st.markdown(f':red[{task_info["task_rem"]}]')                                
-                            st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}')                  
-                            st.markdown(f'Время начала обработки: {task_info["start_processing_timestamp"][:19]}')   
-                            st.markdown(f'Время окончания обработки: {task_info["task_progress_timestamp"][:19]}') 
-                   
+                            st.markdown(f':red[{task_info["task_rem"]}]')
+                            st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}')
+                            st.markdown(f'Время начала обработки: {task_info["start_processing_timestamp"][:19]}')
+                            st.markdown(f'Время окончания обработки: {task_info["task_progress_timestamp"][:19]}')
 
-                                        
                         elif task_info['task_status'] == 'processing':
                             st.markdown(f'#### :blue[{current_user_id}-{task_id[0]}]')
                             st.markdown(f'### :blue[{task_info["file_name"]} обрабатывается]')
                             if task_info['task_progress']:
                                 st.progress(int(task_info['task_progress']))
                             st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}<br>'
-                                    f'Время начала обработки: {task_info["start_processing_timestamp"][:19]}')                    
-                        
-                
+                                    f'Время начала обработки: {task_info["start_processing_timestamp"][:19]}')
+
                         elif task_info['task_status'] == 'ready':
                             st.markdown(f'#### :green[{current_user_id}-{task_id[0]}]')
                             st.markdown(f'### :green[{task_info["file_name"]} обработан]')
                             st.markdown(f'Время постановки в очередь: {task_info["task_timestamp"][:19]}')
                             st.markdown(f'Время начала обработки: {task_info["start_processing_timestamp"][:19]}')
-                            st.markdown(f'Время окончания обработки: {task_info["task_progress_timestamp"][:19]}') 
+                            st.markdown(f'Время окончания обработки: {task_info["task_progress_timestamp"][:19]}')
 
-
-                                            
         await asyncio.sleep(2)
 
-        
+
 def show_ready_user_tasks(current_user_id):
-            tasks_ids = get_tasks_ids(current_user_id, "'ready', 'error'")
-            if tasks_ids:
-                for task_id in tasks_ids:
-                    if f"{task_id[0]}" in st.session_state:
-                        if st.session_state[f"{task_id[0]}"]:
-                            complit_task(task_id[0])
+    tasks_ids = get_tasks_ids(current_user_id, "'ready', 'error'")
+    if tasks_ids:
+        for task_id in tasks_ids:
+            if f"{task_id[0]}" in st.session_state:
+                if st.session_state[f"{task_id[0]}"]:
+                    complit_task(task_id[0])
 
-                    task_info = get_task_info(task_id[0])
-                    if task_info['task_status'] != 'complited':
-                        with st.container(border=True):
+            task_info = get_task_info(task_id[0])
+            if task_info['task_status'] != 'complited':
+                with st.container(border=True):
 
-                            
-                            if task_info['task_status'] == 'error':                    
-                                st.markdown(f'### :red[{current_user_id}-{task_id[0]} {task_info["file_name"]}]')   
-                                st.markdown(f':red[{task_info["task_rem"]}]')                                                            
-                                col1, col2 = st.columns(2)
-                                with col1:
-                                    st.button("❌ Завершить задачу", key=f'{task_info["task_id"]}', use_container_width=True)
-                                if task_info["task_rem"] != 'Already was puted in queue':
-                                    with col2:
-                                        st.button("🐞 Отправить отчет об ошибке", key=f'send_err_{task_info["task_id"]}', use_container_width=True, disabled = True)                                    
+                    if task_info['task_status'] == 'error':
+                        st.markdown(f'### :red[{current_user_id}-{task_id[0]} {task_info["file_name"]}]')
+                        st.markdown(f':red[{task_info["task_rem"]}]')
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            st.button("❌ Завершить задачу",
+                                        key=f'{task_info["task_id"]}',
+                                        use_container_width=True)
+                        if task_info["task_rem"] != 'Already was puted in queue':
+                            with col2:
+                                st.button("🐞 Отправить отчет об ошибке", key=f'send_err_{task_info["task_id"]}', use_container_width=True, disabled=True)
 
-                                                        
-                            elif task_info['task_status'] == 'ready':
-                                st.markdown(f'### :green[{current_user_id}-{task_id[0]} {task_info["file_name"]}]')                                         
-                                col1, col2 = st.columns(2)
-                                with col1:
-                                    st.button("❌ Завершить задачу", key=f'{task_info["task_id"]}', use_container_width=True)                               
-                            
-                                with col2:
-                                    with open(os.path.join(os.getcwd(), 'data', f"{current_user_id}", 'output', f"{task_info['file_name']}.docx"), 'rb') as f:
-                                        st.download_button("📥 Загрузить результат",  f, use_container_width=True, key=f'ready_download_{task_info["task_id"]}', file_name=f'{task_info["file_name"]}.docx')
+                    elif task_info['task_status'] == 'ready':
+                        st.markdown(f'### :green[{current_user_id}-{task_id[0]} {task_info["file_name"]}]')
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            st.button("❌ Завершить задачу", key=f'{task_info["task_id"]}', use_container_width=True)
 
-
-
-                                            
+                        with col2:
+                            with open(os.path.join(os.getcwd(),
+                                                   'data',
+                                                    f"{current_user_id}",
+                                                    'output',
+                                                    f"{task_info['file_name']}.docx"), 'rb') as file_to_download:
+                                st.download_button("📥 Загрузить результат",
+                                                    file_to_download,
+                                                    use_container_width=True,
+                                                    key=f'ready_download_{task_info["task_id"]}',
+                                                    file_name=f'{task_info["file_name"]}.docx')
